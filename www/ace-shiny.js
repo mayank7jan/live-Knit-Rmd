3a9e46f1ff465ce5b568e74db8dc074906cf03c3
Mayank <mayank7jan@gmail.com>
2021-06-05 20:47:58 +0530

    update shiny-ace.js

--- www/ace-shiny.js
+++ www/ace-shiny.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
+$(document).on('shiny:connected', function(event) {
+  $(document).ready(function() {
+    
+    var editor = ace.edit("rmd");
+    
+    editor.commands.addCommand({
+      name: 'insertChunk',
+      bindKey: 'Ctrl-Alt-I',
+      exec: function(editor) {
+        editor.insert('```{r}\n\n```\n');
+        editor.navigateUp(2);
+      }
+    });
   
-  var editor = ace.edit("rmd");
-
-  editor.commands.addCommand({
-    name: 'insertChunk',
-    bindKey: 'Ctrl-Alt-I',
-    exec: function(editor) {
-      editor.insert('```{r}\n\n```\n');
-      editor.navigateUp(2);
-    }
   });
-
 });
