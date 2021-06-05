5374da19f57edde1f73718c68097d8aac4558a5e
Mayank <mayank7jan@gmail.com>
2021-06-05 23:37:06 +0530

    Change icon for HTML file download button

--- www/shiny.js
+++ www/shiny.js
@@ -2,3 +2,14 @@ Shiny.addCustomMessageHandler("element_display_id",
   function(ele_i) {
      document.getElementById(ele_i.id).style.display = ele_i.display;
 });
+
+
+/*Change icon for HTML file download button*/
+
+$(document).on('shiny:connected', function(event) {
+  $(document).ready(function() {
+    var downloadHTML = document.getElementById('DownloadHTML');
+    var HTMLinner = downloadHTML.getElementsByTagName('i');
+    HTMLinner[0].className = "fa fa-file-download";
+  });
+});
